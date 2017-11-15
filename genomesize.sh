82f73f82fe2fffb956060495cf2d27c9fd7b13a2
Raymond Kiu <raymond.kiu@quadram.ac.uk>
2017-11-15 17:53:45 +0000

    Create genomesize.sh

---
+++ genomesize.sh
@@ -0,0 +1,15 @@
+#usage: ./genomesize.sh FASTAFILE
+#FASTAFILE is the assembly file in fasta format
+#This will give a final output named 'FASTAFILE-genomesize', with all intermediate files cleared
+
+contigs=$1
+
+
+awk '!/^>/ { printf "%s", $0; n = "\n" } /^>/ { print n $0; n = "" } END { printf "%s", n }' $contigs > single
+cat single | awk 'NR%2==0' | awk '{print length($1)}' > sum
+awk '{ sum += $1 } END { print sum }' sum > $contigs-genomesize
+
+rm single
+rm sum
+
+exit 0;
